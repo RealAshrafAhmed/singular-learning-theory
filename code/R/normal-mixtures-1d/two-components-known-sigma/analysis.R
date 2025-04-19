d5616d87fb002a74a4601e4a617ae6cbb6e54a67
Ashraf Ahmed <ashrafahmed@sophi.io>
2025-04-19 11:03:32 -0400

    added GE estimates

--- code/R/normal-mixtures-1d/two-components-known-sigma/analysis.R
+++ code/R/normal-mixtures-1d/two-components-known-sigma/analysis.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 library(ggplot2)
+library(ggh4x)
 library(data.table)
 source("./two-components-known-sigma/globals.R")
 
@@ -22,7 +23,6 @@ n_factors = unique(fe_data$cn)
 fe_true = rep(0, length(n_factors))
 for(i in 1:length(n_factors)) {
   n = n_factors[i]
-  print(n)
   observationsfile <- paste0(basedir, "/data/observations-n", n, ".csv")
   data = as.matrix(read.table(observationsfile, sep= ",",header=TRUE))[,1]
   Sn = -1*mean(dnorm(data, mean=0, sd=1, log=TRUE))
@@ -34,15 +34,22 @@ y_intercepts <- data.frame(
   FE = fe_true
 )
 
+# custom ylimits for fee energy. too many outliers from the MAP estimator
+y_scales_list <- list(
+  "10" = scale_y_continuous(limits = c(15, 18)),
+  "100" = scale_y_continuous(limits = c(150, 157))
+)
 
-ggplot(fe_data, aes(x=factor(cchain_size), y=cFE, color=cname))+geom_boxplot()+
-  # geom_hline(yintercept = 3/4, color="red")+
+ggplot(fe_data, aes(x=factor(cchain_size), y=cFE, color=cname))+
+  geom_boxplot()+
   geom_hline(data = y_intercepts, aes(yintercept = FE), color = "red", linetype = "dashed") +
-  facet_wrap(~factor(cn), scales = "free_y")
+  facet_wrap(~factor(cn), scales = "free_y")+
+  facetted_pos_scales(y = y_scales_list)
 
 
 datafile <- paste0(basedir, "/data/ge-estimates.csv")
 ge_data <- as.data.table(read.table(datafile, sep= ",",header=TRUE))
 
-ggplot(ge_data, aes(x=factor(cn), y=ge, color=factor(cn)))+
-  geom_boxplot()
+ggplot(ge_data, aes(x=factor(cname), y=ge, color=factor(cname)))+
+  geom_boxplot()+
+  facet_wrap(~factor(cn))

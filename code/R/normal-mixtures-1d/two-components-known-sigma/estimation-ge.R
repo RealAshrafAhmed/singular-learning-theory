10c17a174d83a4ce60ea417102c0df38e3e914b1
Ashraf Ahmed <ashrafahmed@sophi.io>
2025-04-24 10:03:38 -0400

    added Cn to ge estimates

--- code/R/normal-mixtures-1d/two-components-known-sigma/estimation-ge.R
+++ code/R/normal-mixtures-1d/two-components-known-sigma/estimation-ge.R
@@ -9,17 +9,19 @@ source(paste0(basedir, "/fit.R"))
 # ******************************************
 
 # observations sample size
-n_factors = c(10, 100, 250)
+n_factors = c(10, 100, 250, 1000)
+total_sims = 1000
 n_test = 1000
 
 # generate the samples and save them. that way we can rerun the simulations without worry about 
 # data variability
 for(n in n_factors) {
-  datafile <- paste0(basedir, "/data/observations-n", n, ".csv")
+  datafile <- paste0(basedir, "/data/observations-n", n, "-times", total_sims,".csv")
   if (file.exists(datafile)) { # only create data if they don't already exist
     print(sprintf("Skipping file %s, it already exists!", datafile))
   } else {
-    fwrite(data.frame(x=rnorm(n=n, mean=0, sd=1)),
+    data = matrix(data=rnorm(n*total_sims, mean=0, sd=1), nrow=n, ncol=total_sims)
+    fwrite(data.frame(data),
            file = datafile, 
            col.names=TRUE, row.names = FALSE)
   }
@@ -77,20 +79,19 @@ if (file.exists(datafile)) {
 print(sprintf("Created file %s to store free energy estimates", datafile))
 
 
-chain_size = 10000
-total_sims = 100
-estimators = c("Gn", "Tn")
+chain_size = 6000
+estimators = c("Gn", "Tn", "Cn")
 
 pb = txtProgressBar(min = 0, max = total_sims, initial = 0)
 
-for(n in n_factors) {
-  observationsfile <- paste0(basedir, "/data/observations-n", n, ".csv")
-  data = as.matrix(read.table(observationsfile, sep= ",",header=TRUE))[,1]
-  
-  testfile <- paste0(basedir, "/data/test-n", n_test, ".csv")
-  test_data = as.matrix(read.table(testfile, sep= ",",header=TRUE))[,1]
-  for(i in 1:total_sims) { # repeat for total_sims conditions to approx estimator variance
+for(i in 1:total_sims) { # repeat for total_sims conditions to approx estimator variance
+  for(n in n_factors) {
     for(estimator in estimators) {
+      observationsfile <- paste0(basedir, "/data/observations-n", n, "-times", total_sims, ".csv")
+      data = as.matrix(read.table(observationsfile, sep= ",",header=TRUE))
+      testfile <- paste0(basedir, "/data/test-n", n_test, ".csv")
+      test_data = as.matrix(read.table(testfile, sep= ",",header=TRUE))[,1]
+
       # check if the data has already been generated and saved in the file
       match = data_sofar[ctrial==i & cn==n & cchain_size==chain_size & cname==estimator]
       # print(match)
@@ -105,18 +106,18 @@ for(n in n_factors) {
       
       more_estimates = NA
       
-      model_fit = fitstan(data=data,
-                          beta=1,
-                          model=model,
-                          size=chain_size,
-                          warmup=2000, # be careful when changing this, consult chain size analysis
-                          chains=2)    # number of chains to compute \hat{\lambda}^m
-  
-      draws = as.data.table(extract(model_fit$fit,
-                                    par=c("rho", "mu[1]", "mu[2]"),
-                                    permuted=TRUE))
-      
       if(estimator == "Gn") {
+        model_fit = fitstan(data=data[,i],
+                            beta=1,
+                            model=model,
+                            size=chain_size,
+                            warmup=4000, # be careful when changing this, consult chain size analysis
+                            chains=2)    # number of chains to compute \hat{\lambda}^m
+        
+        draws = as.data.table(extract(model_fit$fit,
+                                      par=c("rho", "mu[1]", "mu[2]"),
+                                      permuted=TRUE))
+        
         pred_lpdf = predictive_lpdf(draws)
         test_data_lpdf = sapply(test_data, FUN=pred_lpdf)
         true_data_lpdf = dnorm(test_data, mean=0, sd=1, log=TRUE)
@@ -129,8 +130,18 @@ for(n in n_factors) {
                                     cchain_size=chain_size,
                                     cname="Gn")
       } else if(estimator == "Tn") {
+        model_fit = fitstan(data=data[,i],
+                            beta=1,
+                            model=model,
+                            size=chain_size,
+                            warmup=4000, # be careful when changing this, consult chain size analysis
+                            chains=2)    # number of chains to compute \hat{\lambda}^m
+        
+        draws = as.data.table(extract(model_fit$fit,
+                                      par=c("rho", "mu[1]", "mu[2]"),
+                                      permuted=TRUE))
         pred_lpdf = predictive_lpdf(draws)
-        test_data_lpdf = sapply(data, FUN=pred_lpdf)
+        test_data_lpdf = sapply(data[,i], FUN=pred_lpdf)
         
         # compute the generalization error estimate
         more_estimates = data.table(ctrial = i,
@@ -138,6 +149,32 @@ for(n in n_factors) {
                                     cn=n,
                                     cchain_size=chain_size,
                                     cname="Tn")
+      } else if(estimator == "Cn") {
+        # leave one out estimate
+        # we need to drop a value and refit and take the average
+        estimate = rep(0, n)
+        for(j in 1:n) {
+          model_fit = fitstan(data=data[,i][-j],
+                              beta=1,
+                              model=model,
+                              size=chain_size,
+                              warmup=4000, # be careful when changing this, consult chain size analysis
+                              chains=1)    # number of chains to compute \hat{\lambda}^m
+          
+          draws = as.data.table(extract(model_fit$fit,
+                                        par=c("rho", "mu[1]", "mu[2]"),
+                                        permuted=TRUE))
+          pred_lpdf = predictive_lpdf(draws)
+          test_data_lpdf = sapply(data[,i], FUN=pred_lpdf)
+          estimate[j] = mean(-1*test_data_lpdf)
+          print(paste0("estimating cross validation ", n, "\\", j ," dropout out"))
+        }
+        # compute the generalization error estimate
+        more_estimates = data.table(ctrial = i,
+                                    ge = mean(estimate),
+                                    cn=n,
+                                    cchain_size=chain_size,
+                                    cname="Cn")
       }
   
       fwrite(more_estimates, 

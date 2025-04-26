d57e54fc5064af0ebad0b0f57465b3e0e82f69d1
Ashraf Ahmed <ashrafahmed@sophi.io>
2025-04-26 11:43:18 -0400

    added mse analysis

--- code/R/normal-mixtures-1d/two-components-known-sigma/rlct-mse-analysis.R
+++ code/R/normal-mixtures-1d/two-components-known-sigma/rlct-mse-analysis.R
@@ -2,12 +2,14 @@ rm(list=ls())
 library(ggplot2)
 # library(ggh4x)
 library(data.table)
+library(ggthemes)
 source("./two-components-known-sigma/globals.R")
 
 datafile <- paste0(basedir, "/data/results/rlct-m1.csv")
 rlctdf <- read.table(datafile, sep= ",",header=TRUE)
 
-cfactors = unique(rlctdf$c)
+# cfactors = unique(rlctdf$c)
+cfactors = c(1., 2.)
 nfactors = unique(rlctdf$n)
 max_chainsize = max(rlctdf$chain_size)
 
@@ -37,11 +39,13 @@ for(c in cfactors) {
 }
 
 
-ggplot(mse_data, aes(x = factor(n), y = factor(c), fill = mse)) +
-  geom_tile()
+ggplot(mse_data, aes(x = factor(n), y = factor(c), fill = bias)) +
+  geom_tile() + 
+  theme_wsj() + scale_fill_gradient2(low = "blue", mid = "white", high = "red")
 
-ggplot(mse_data, aes(x=factor(n), y=mse, group=factor(c)))+
-  geom_line(color="red") + scale_y_log10()
 
-ggplot(mse_data, aes(x=factor(n), y=variance, group=factor(c)))+
-  geom_line(color="orange")
+ggplot(mse_data, aes(x=factor(n), y=mse, group=factor(c), color=factor(c)))+
+  geom_line() + theme_hc()
+
+ggplot(mse_data, aes(x=factor(n), y=mse, group=factor(c), color=factor(c)))+
+  geom_line()

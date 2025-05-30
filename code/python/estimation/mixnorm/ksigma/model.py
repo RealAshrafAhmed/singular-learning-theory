1f7aa00f43ddf20c0ca37c20267f2f0b14b6e6f5
Ashraf Ahmed <ashrafahmed@sophi.io>
2025-05-29 22:14:41 -0400

    tempered normal mixture

--- code/python/estimation/mixnorm/ksigma/model.py
+++ code/python/estimation/mixnorm/ksigma/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pymc as pm
 from pymc import logp
+<<<<<<< HEAD:code/python/estimation/mixnorm/ksigma/model.py
 import pytensor as pt
 
 # inverse temperature factor
@@ -9,11 +10,17 @@ def tempered_normal_mixture(beta, data,
                             weights_prior_alpha=np.full(3, 0.1),
                             mean_prior_mu=pt.tensor.zeros((3,)),
                             mean_prior_cov=pt.tensor.eye(3)*2):
+=======
+
+# inverse temperature factor
+def tempered_normal_mixture(beta, data, n_components=3):
+>>>>>>> 73837af (tempered normal mixture):code/python/normal-mixtures-1d/known-sigma/model.py
     if not beta:
         likelihood_power = 1/np.log(len(data))
     else:
         likelihood_power = beta
 
+<<<<<<< HEAD:code/python/estimation/mixnorm/ksigma/model.py
     model = pm.Model()
     with model:
         # Priors for mixture weights
@@ -31,10 +38,35 @@ def tempered_normal_mixture(beta, data,
     
         # Priors for component means
         mus = pm.MvNormal("mus", mu=mean_prior_mu, cov=mean_prior_cov)
+=======
+    basic_model = pm.Model()
+    with basic_model:
+        # Priors for mixture weights
+        # Dirichlet distribution ensures weights sum to 1
+        weights = pm.Dirichlet("rho", np.ones(n_components))
+    
+        # Priors for component means
+        mus = pm.Normal("mu",
+                        mu=0,  # Centered around the data mean
+                        sigma=10,        # Broad prior
+                        shape=n_components)
+    
+        # X_obs = pm.NormalMixture("X_obs", 
+                                 # w=weights, 
+                                 # mu=mus,
+                                 # sigma=np.ones(3),
+                                 # observed=x_data,
+                                 # weight=c/np.log(len(x_data))
+    
+>>>>>>> 73837af (tempered normal mixture):code/python/normal-mixtures-1d/known-sigma/model.py
         like = pm.NormalMixture("like", w=weights, mu=mus, sigma=1)
         # print(f"Type of mixture_dist: {type(mixture_dist)}")
         # print(f"Does mixture_dist have 'logp' attribute? {'logp' in dir(mixture_dist)}")
         tempered_log_likelihood = likelihood_power * logp(like, data)
         pm.Potential("tempered_likelihood", tempered_log_likelihood)
+<<<<<<< HEAD:code/python/estimation/mixnorm/ksigma/model.py
         # metropolis_step_mu = pm.Metropolis(vars=[weights])
     return model
+=======
+    return basic_model
+>>>>>>> 73837af (tempered normal mixture):code/python/normal-mixtures-1d/known-sigma/model.py

c48e0cc8b1f3782495e97e00723108f420278abe
Ashraf Ahmed <ashrafahmed@sophi.io>
2025-11-27 20:16:51 -0500

    reorganized repo for uv

--- code/R/normal-mixtures-1d/two-components-known-sigma/globals.R
+++
@@ -1,7 +0,0 @@
-basedir <- './two-components-known-sigma'
-source("lib/stan_utilities.R")
-
-# set.seed(333) # for reproducability between machines
-
-# set the parallelism based on # of cores of the machine
-options(mc.cores=parallel::detectCores())

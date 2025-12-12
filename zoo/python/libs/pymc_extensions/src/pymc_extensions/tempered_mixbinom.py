f04b27a67b30c9781af706d0ba95989afa90854d
Ashraf Ahmed <ashrafahmed@sophi.io>
2025-12-11 22:56:01 -0500

    sequential ti

--- zoo/python/libs/pymc_extensions/src/pymc_extensions/tempered_mixbinom.py
+++ zoo/python/libs/pymc_extensions/src/pymc_extensions/tempered_mixbinom.py
@@ -93,19 +93,19 @@ def __approx_tempered_nll(X, n_trials, beta, nuts_sampler):
       return model.wbic(idata)
 
 
-# def free_energy(n_trials, X, betas, nuts_sampler="nutpie"):
-#   """Compute free energy using thermodynamic integral"""
-#   if len(betas)==0:
-#     betas = np.linspace(0, 1, 30) **2
+def free_energy(n_trials, X, betas, nuts_sampler="nutpie"):
+  """Compute free energy using thermodynamic integral"""
+  if len(betas)==0:
+    betas = np.linspace(0, 1, 30) **2
 
-#   wbic_betas=[]
-#   for beta in betas:
-#     wbic_betas.append(__approx_tempered_nll(X, n_trials, beta, nuts_sampler))
+  wbic_betas=[]
+  for beta in betas:
+    wbic_betas.append(__approx_tempered_nll(X, n_trials, beta, nuts_sampler))
 
-#   # Compute integration numerically using the trapezoidal rule
-#   return np.trapz(wbic_betas, betas)
+  # Compute integration numerically using the trapezoidal rule
+  return np.trapz(wbic_betas, betas)
 
-def free_energy(n_trials, X, betas, parallel_n_jobs=1, parallel_verbose=10, nuts_sampler="nutpie"):
+def free_energy_parallel(n_trials, X, betas, parallel_n_jobs=1, parallel_verbose=5, nuts_sampler="nutpie"):
   """Compute free energy using thermodynamic integral"""
   if len(betas)==0:
     betas = np.linspace(0, 1, 30) **2
